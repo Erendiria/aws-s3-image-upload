828c24afdd5170e6ecded731a92d9e4f86ab19bf
Erendiria <151480543+Erendiria@users.noreply.github.com>
2023-12-15 18:11:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import serverless_wsgi
+
 from flask import Flask
 from flask_jwt_extended import JWTManager 
 from flask_restful import Api
 from config import Config
+from resources.image import FileUploadResource
+from resources.posting import PostingResource
+from resources.rekognition import ObjectDetectionResource
 
 app = Flask(__name__)
 
@@ -13,6 +18,13 @@ jwt = JWTManager(app)
 
 api = Api(app)
 
+api.add_resource( FileUploadResource , '/upload')
+api.add_resource( PostingResource , '/posting')
+api.add_resource( ObjectDetectionResource , '/object_detection')
+
+
+def handler(event, context) :
+    return serverless_wsgi.handle_request(app, event, context )
 
 if __name__ == '__main__':
     app.run()
